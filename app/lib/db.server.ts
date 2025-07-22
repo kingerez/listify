13d766d6c144c42a6f897b1e87cd21588df34681
Erez <erez.r@minutemedia.com>
2025-07-22 12:30:50 +0300

    add default mongodb uri

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
+const MONGODB_URI = process.env.MONGODB_URI! || "mongodb://localhost:27017/mydatabase";
 
-if (!process.env.MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env"
-  );
-}
+// if (!process.env.MONGODB_URI) {
+//   throw new Error(
+//     "Please define the MONGODB_URI environment variable inside .env"
+//   );
+// }
 
 /**
  * Global is used here to maintain a cached connection across hot reloads
